0bcad0093a389cf27660777819c2a2eb51e1b3bf
Amr Suliman <amrelgohary573@gmail.com>
2026-09-22 16:40:34 +0300

    Question 01

--- AssignmentFiveBasics/Program.cs
+++ AssignmentFiveBasics/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            #region Question 1
+
+            // private members are accessible only inside the class where they are declared.
+            // So, password cannot be accessed from Main because Main is outside the Book class.
+
+            // Book book = new Book();
+            // Console.WriteLine(book.password); // Error: 'password' is inaccessible due to its protection level
+
+            #endregion
+
         }
     }
 }
